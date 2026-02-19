7b5fed00b43cfedc4c5869ed9ae519b80b98dafc
carlosbastoslaet <carlosbastoslaet@gmail.com>
2026-02-18 21:17:09 -0300

    fix tf

--- data.tf
+++ data.tf
@@ -1,14 +1,14 @@
 data "aws_s3_bucket" "video_uploads" {
   bucket = "video2frames-video-uploads"
-  outputs = {
-    api_gateway_id = "hackathon_api"
+}
+
+data "terraform_remote_state" "infra" {
+  backend = "s3"
+  config = {
+    bucket = "tc-2025-tf-states-bucket"
+    key    = "PRD/tc-infra"
+    region = "us-east-1"
   }
-  tags = merge(
-    var.tags,
-    {
-      Name = "Video Uploads Bucket"
-    }
-  )
 }
 
 data "terraform_remote_state" "database" {
@@ -21,7 +21,7 @@ data "terraform_remote_state" "database" {
 }
 
 data "aws_apigatewayv2_api" "hackathon_api" {
-  api_id = data.aws_s3_bucket.video_uploads.outputs.api_gateway_id
+  api_id = data.terraform_remote_state.infra.outputs.api_gateway_id
 }
 
 data "aws_vpc" "tc_lambda_vpc" {
@@ -32,7 +32,6 @@ data "aws_vpc" "tc_lambda_vpc" {
 }
 
 data "aws_subnets" "tc_lambda_subnets" {
-  depends_on = [data.aws_vpc.tc_lambda_vpc]
   filter {
     name   = "vpc-id"
     values = [data.aws_vpc.tc_lambda_vpc.id]
